d6f35cd2da77d60baa12cc35cb74509285c2bcab
Kidir Torain <kktorainnn@gmail.com>
2020-07-31 18:31:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+#one player pong game
+#Paddle, Ball, and 3 Walls. 
+#move up and down 
+#100 pixels in height, and 15 pixels in width
+#ball and paddle should both properly collide as well as the ball and 3 walls to its left
+#ball ever touches the rightmost wall of the screen the game loop should end completely
+
+import pygame
+pygame.init()
+screen_width = 15
+screen_height = 100
+ball_size = 25 
+
+def construct_ball():
+  ball= ball()
+  ball.x 
+  ball.y
+
+
+
+
+class Ball:
+  def __init__(self):
+        self.x =
+        self.y = 
+        self.change_x = 
+        self.change_y = 
+full_circle()
+outline_circle()
+draw_image(10, 20, "ball.png")
+
+
+
+
+
+
+
+# If the ball ever touches the rightmost wall of the screen the game loop should end completely. 
+ 
+
+
+#resource used- http://programarcadegames.com/python_examples/f.php?file=bouncing_balls.py https://sites.google.com/site/thepythonpongtutorial/drawing-functions/drawing-functions
