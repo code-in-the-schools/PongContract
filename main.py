1f004d7db7358b041e78f2b4a93e78c7262d3c84
Kidir Torain <kktorainnn@gmail.com>
2020-08-05 15:45:50 +0000

    coding

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
   screen_width = 15
   screen_height = 100
   ball_size = 25 
+  screen = pygame.time.Clock()
 class Ball(object):
   def __init__(self):
         self.x = 10 
@@ -25,11 +26,28 @@ class Ball(object):
           ball.x = 10
           ball.y = 10
         return ball
+  def movement(self, width, height):
+      key = pygame.key.get_pressed()
+      if key[pygame.K_DOWN]:
+  
+        if key[pygame.K_UP]:
+
+          if key[pygame.K_RIGHT]:
+
+            if key[pygame.K_LEFT]:
+              while:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit():
+        ball = ball.move(speed)
+        if ball.left < 0 or ball.right > width:
+            speed[0] = -speed[0]
+        if ball.top < 0 or ball.bottom > height:
+            speed[1] = -speed[1] 
 
   def display(self):
         pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
 
-class paddle(object):
+class Paddle(object):
   def __init_(self):
     self.x = 15
     self.y = 20
@@ -40,35 +58,37 @@ def construct_paddle():
     paddle.y = 15
  
     return paddle
-
-    def movement(self, width, height):
-      key = pygame.key.get_pressed()
-    if key[pygame.K_DOWN]:
-  
-      if key[pygame.K_UP]:
-
-        if key[pygame.K_RIGHT]:
-
-          if key[pygame.K_LEFT]:
-    while :
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit():
-        ball = ball.move(speed)
-        if ball.left < 0 or ball.right > width:
-            speed[0] = -speed[0]
-        if ball.top < 0 or ball.bottom > height:
-            speed[1] = -speed[1] 
+  if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    player_paddle.direction = -1
+                elif event.key == K_DOWN:
+                    player_paddle.direction = 1
+            if event.type == KEYUP:
+                if event.key == K_UP and player_paddle.direction == -1:
+                    player_paddle.direction = 0
+                elif event.key == K_DOWN and player_paddle.direction == 1:
+                    player_paddle.direction = 0
 
 def bounce(self,screen_width,screen_height):
   self.x += self.x_velocity 
   self.y += self.y_velocity
-  
+if (self.y<= 0)=1
+  self.y_velocity = 1
+  if (self.y>= screen_height-50):
+  self.y_velocity = -1
+  if (self.x<=0):
+    self.x_velocity =1 
+    if (self.x_velocity>= screen_width-50):
+      self.x_velocity= -1 
+
 #If the ball ever touches the rightmost wall of the screen the game loop should end completely. 
   running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
          pygame.quit()
-                quit()   
+                
 
 #resource used- http://programarcadegames.com/python_examples/f.php?file=bouncing_balls.py https://sites.google.com/site/thepythonpongtutorial/drawing-functions/drawing-functions
+#http://programarcadegames.com/python_examples/f.php?file=pong.py
+#https://codereview.stackexchange.com/questions/144868/1-player-pong-with-ai
